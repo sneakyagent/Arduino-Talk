c706f8c8a98ef9b363a4de69a355440e2afe26bf
sneakyagent <31023605+sneakyagent@users.noreply.github.com>
2019-04-13 09:34:24 -0600

    Add files via upload

---
+++ sensor.ino
@@ -0,0 +1,36 @@
+#include <QTRSensors.h>
+//left one green
+int sensorRightPin = A0;    // select the input pin 
+//right one yeelow
+int sensorLeftPin = A2;    // select the input pin 
+
+int sensorValueLeft = 0;  // variable to store the value coming from the sensor
+int sensorValueRight = 0;  // variable to store the value coming from the sensor
+int cntrl =  A3;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // read the value from the sensor:
+       sensorValueRight = analogRead(sensorRightPin);
+        sensorValueLeft = analogRead(sensorLeftPin);
+  if(sensorValueRight >700)
+  {
+     Serial.println("right");
+  }
+  else if  (sensorValueLeft > 700)
+  {
+    Serial.println("left");
+  }
+  else
+  {
+    Serial.println("STRIGHT");
+  }
+   
+ // delay(100); // the number of milliseconds between readings                 
+}
+
---
+++ servo.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo servoL;
+int pinOutServoL = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  servoL.attach(pinOutServoL); 
+}
+
+void loop() {
+  servoL.write(-255);
+  delay(0);
+ 
+
+}
